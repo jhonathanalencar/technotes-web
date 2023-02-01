e179c664e2b469e40473e66603e583039a4042df
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-02-01 20:14:09 -0300

    fix: unsubscribe users on component unmount

--- src/features/auth/Prefetch.tsx
+++ src/features/auth/Prefetch.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { store } from '../../redux/store';
 import { usersApiSlice } from '../users/usersApiSlice';
 
 export function Prefetch() {
-  store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
+  useEffect(() => {
+    const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
+
+    return () => {
+      users.unsubscribe();
+    };
+  }, []);
 
   return <Outlet />;
 }
