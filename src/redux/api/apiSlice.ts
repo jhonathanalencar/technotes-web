096f7004fa12ce41ff4713360193bb73a2bbb599
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-02-05 15:24:40 -0300

    feat: use jwt token to request data from backend

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,9 +1,66 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+  BaseQueryApi,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react';
+
+import { setCredentials } from '../../features/auth/authSlice';
+import { QueryError } from '../../shared/types';
+
+import { RootState } from '../store';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'http://localhost:3500',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
+
+interface RefreshQueryResponse {
+  accessToken: string;
+}
+
+async function baseQueryWithReauth(
+  args: string | FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: object
+) {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 403) {
+    const refreshResult = await baseQuery('/auth/refresh', api, extraOptions);
+
+    if (refreshResult?.data) {
+      api.dispatch(
+        setCredentials({ ...(refreshResult.data as RefreshQueryResponse) })
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      if (refreshResult?.error?.status === 403) {
+        const error = refreshResult.error as QueryError;
+        if (error.data) {
+          error.data.error = 'Seu acesso expirou';
+        }
+      }
+
+      return refreshResult;
+    }
+  }
+
+  return result;
+}
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3500',
-  }),
+  baseQuery: baseQueryWithReauth,
   tagTypes: ['Note', 'User'],
   endpoints: () => ({}),
 });
