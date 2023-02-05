6fad203ae198fd136b0aacda73f55e94ef2802cc
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-02-05 16:38:45 -0300

    feat: add usePersist hook

---
+++ src/hooks/usePersist.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+function usePersist() {
+  const [persist, setPersist] = useState(() => {
+    const storedJSON = localStorage.getItem('persist');
+
+    if (storedJSON) {
+      return JSON.parse(storedJSON) as boolean;
+    } else {
+      return false;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@techNotes:0.0.1:persist', JSON.stringify(persist));
+  }, [persist]);
+
+  return [persist, setPersist];
+}
+
+export { usePersist };
