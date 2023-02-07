9babf9d082d1f25dd9870b8a23e886a94a0afef7
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-02-07 19:08:49 -0300

    feat: add RequireAuth Component

--- src/features/auth/RequireAuth.tsx
+++ src/features/auth/RequireAuth.tsx
@@ -1,11 +1,21 @@
-import { useLocation } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
-export function RequireAuth() {
+import { useAuth } from '../../hooks/useAuth';
+import { Role } from '../../shared/types';
+
+interface RequireAuthProps {
+  allowedRoles: Role[];
+}
+
+export function RequireAuth({ allowedRoles }: RequireAuthProps) {
   const location = useLocation();
   const { roles } = useAuth();
 
-  const content = roles.some();
+  const content = roles.some((role) => allowedRoles.includes(role)) ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 
   return content;
 }
