cc9d9efbf00f54bd385551e41ad4d1632b6148d5
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-02-08 17:38:41 -0300

    feat: disable react and redux devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import { store } from './redux/store';
 
 import { App } from './App';
 
 import './index.css';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
